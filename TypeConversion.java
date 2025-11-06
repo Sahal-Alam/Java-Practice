a80c89a62b6654d2dc52c03abe463f28075783e9
Sahal-Alam <117425323+Sahal-Alam@users.noreply.github.com>
2025-11-06 09:13:55 +0600

    Add files via upload

---
+++ TypeConversion.java
@@ -0,0 +1,10 @@
+public class TypeConversion {
+    static void main() {
+        int i = 5;
+
+        System.out.println(i);
+
+        int i1 = (int) 5.0;
+        System.out.println(i1);
+    }
+}
