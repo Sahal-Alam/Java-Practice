01fd856bd6814c26fb8e3ba42aa10fcfa05ade49
Sahal-Alam <117425323+Sahal-Alam@users.noreply.github.com>
2025-11-28 08:45:20 +0600

    Add files via upload

---
+++ Method/src/Multiplication.java
@@ -0,0 +1,9 @@
+public class Multiplication {
+    static void main(String[] args) {
+        mul(3,5);
+        mul(5,10);
+    }
+    public static void mul(int num1 , int num2){
+        System.out.println("The multiplication of " +num1+ " and "+num2+ " is: "+(num1 * num2));
+    }
+}
